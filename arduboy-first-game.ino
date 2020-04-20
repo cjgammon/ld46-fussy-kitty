b3217ab3d479c7308bd3045f4740514849402c0c
cjgammon <gammon@adobe.com>
2020-04-19 21:24:42 -0500

    add resources

--- arduboy-first-game.ino
+++ arduboy-first-game.ino
@@ -1,15 +1,4 @@
-/*
-Hello, World! example
-June 11, 2015
-Copyright (C) 2015 David Martinez
-All rights reserved.
-This code is the most basic barebones code for writing a program for Arduboy.
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-*/
 
 #include <Arduboy2.h>
 
